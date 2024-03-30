5748dc475660994fea5fb9df288d85301889a218
srilathadinakar <srilathadinakar@gmail.com>
2024-03-30 17:39:24 +0530

    updated

---
+++ script.js
@@ -0,0 +1,28 @@
+/*const detail = {
+    name : "test",
+    age : 5
+}
+
+const data = Object.entries(detail)
+console.log(data);
+
+/*for(var [key,value] of data){
+    console.log(value)
+}*/
+
+/*data.forEach(([key,value])=>console.log(value));*/
+
+//const arr=[""]
+
+const resume = {
+    "name":"srilatha",
+    "age":"36",
+    "language":["tamil","english"],
+    "address":{
+        "city":"salem",
+        "state":"TN"
+    }
+
+}
+const data = Object.entries(resume)
+console.log(data);
